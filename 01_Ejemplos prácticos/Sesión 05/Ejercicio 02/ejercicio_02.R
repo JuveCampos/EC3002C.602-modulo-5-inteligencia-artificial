8ce546e04cef98e48baa3c15325a4f3d16221f91
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2025-09-23 18:14:04 -0600

    [20250923] Subida datos

--- 01_Ejemplos prácticos/Sesión 05/Ejercicio 02/ejercicio_02.R
+++ 01_Ejemplos prácticos/Sesión 05/Ejercicio 02/ejercicio_02.R
@@ -21,6 +21,9 @@ print(tablas_disponibles)
 # La función dplyr::tbl() permite crear tablas sql con evaluación floja para explorar los datos
 ventas <- tbl(con, "ventas")
 proveedores <- tbl(con, "proveedores")
+
+
+
 # Realice la evaluación lazy para el resto de las tablas de la base de datos: 
 # Compras_proveedores, detalle_ventas, empleados, nomina, productos, productos_mas_vendidos, proveedores, rendimiento_empleados, resumen_ventas_sucursal, sucursales, ventas
 # Guarde esas tablas en un objeto de R que tenga el mismo nombre que la tabla 
@@ -51,6 +54,45 @@ query_sql_directo <- tbl(con, sql("
 # Con estas funciones, más lo que sabe de su clase del viernes, genere las siguientes gráficas: 
 
 # 1) Los cinco productos más vendidos
+productos_mas_vendidos <- tbl(con, "productos_mas_vendidos")
+tbl(con, sql("
+             SELECT `productos_mas_vendidos`.*
+              FROM `productos_mas_vendidos`
+              ORDER BY -`unidades_vendidas`
+              LIMIT 5
+             "))
+
+query_productos <- productos_mas_vendidos %>% 
+  arrange(-unidades_vendidas) %>% 
+  head(5)
+
+show_query(query_productos)
+
+# tbl(con, sql("
+#   SELECT `ventas`.*
+#   FROM `ventas`
+#   WHERE (`total_venta` > 1000.0)
+#   ORDER BY `total_venta` DESC
+# "))
+
 # 2) Los cinco meseros con más ventas
+print(tablas_disponibles)
+
+ventas <- tbl(con, "ventas")
+empleados <- tbl(con, "empleados")
+
+fusion <- inner_join(ventas, empleados, by = "id_empleado") %>% 
+  group_by(id_empleado, nombre_completo) %>% 
+  summarise(total_ventas = sum(total_venta)) %>% 
+  arrange(-total_ventas) %>% 
+  head(5)
+
+show_query(fusion)
+
+fusion %>% 
+  collect() %>% 
+  View()
+
+
 # 3) Los cinco proveedores con más compras
 
